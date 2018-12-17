9d54629bf9f54cd0fbeea775e3efd345f5619516
wangchenyao <wangchenyao@imdada.cn>
2018-12-17 11:49:36 +0800

    init

--- train.py
+++ train.py
@@ -1,25 +1,24 @@
 #! /usr/bin/env python3
-#-*- coding:utf-8 -*-
+# -*- coding:utf-8 -*-
+
+import argparse
+
+import os
 
 from generate import Generator
-from gensim import models
-from plan import train_planner
 from paths import save_dir
-import argparse
-import os
-import sys
-
+from plan import train_planner
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description = 'Chinese poem generation.')
-    parser.add_argument('-p', dest = 'planner', default = False, 
-            action = 'store_true', help = 'train planning model')
-    parser.add_argument('-g', dest = 'generator', default = False, 
-            action = 'store_true', help = 'train generation model')
-    parser.add_argument('-a', dest = 'all', default = False,
-            action = 'store_true', help = 'train both models')
-    parser.add_argument('--clean', dest = 'clean', default = False,
-            action = 'store_true', help = 'delete all models')
+    parser = argparse.ArgumentParser(description='Chinese poem generation.')
+    parser.add_argument('-p', dest='planner', default=False,
+                        action='store_true', help='train planning model')
+    parser.add_argument('-g', dest='generator', default=False,
+                        action='store_true', help='train generation model')
+    parser.add_argument('-a', dest='all', default=False,
+                        action='store_true', help='train both models')
+    parser.add_argument('--clean', dest='clean', default=False,
+                        action='store_true', help='delete all models')
     args = parser.parse_args()
     if args.clean:
         for f in os.listdir(save_dir):
@@ -29,6 +28,5 @@ if __name__ == '__main__':
             train_planner()
         if args.all or args.generator:
             generator = Generator()
-            generator.train(n_epochs = 1000)
+            generator.train(n_epochs=1000)
         print("All training is done!")
-
